e8b10da24b153bc463a94af8353a5b441a00b845
Kenneth Rose <kennethrose82@gmail.com>
2015-08-16 21:45:29 +0200

    Added a few checks and balances.

--- R/cvr_get.R
+++ R/cvr_get.R
@@ -7,24 +7,42 @@
 #' @export
 cvr_get <- function(search, country, search_in = NULL){
 
+  stopifnot(country == "dk" | country == "no")
+  stopifnot(search_in == "vat" | search_in == "name" | search_in == "produ" | search_in == "phone")
 
+  search <- as.character(search)
+
+  # Create the url
   cvr_url <- "http://cvrapi.dk/api?"
-
   query <- paste0("country=", country, "&format=json")
 
+  # Unless the user have suplied a search_in we just use the standard search
   if(!is.null(search_in)){
     query <- paste0(search_in, "=", cvr_correct_url(URLencode(search)), "&", query)
   } else {
     query <- paste0("search=", cvr_correct_url(URLencode(search)), "&", query)
   }
 
+  # Finish the url
   cvr_url <- paste0(cvr_url, query)
 
-  cvr_response <- httr::GET(cvr_url, httr::add_headers("User-Agent" = "github.com/krose/cvrapi"), httr::verbose())
+  # makee the request and format the content
+  cvr_response <- httr::GET(cvr_url, httr::add_headers("User-Agent" = "github.com/krose/cvrapi"))
+  cvr_content <- httr::content(cvr_response, as = "text")
+  cvr_content <- jsonlite::fromJSON(cvr_content)
 
-  cvr_content <- httr::content(cvr_response, as = "raw")
+  # if the API returns an error, then return an appropiate error message
+  if(!is.null(cvr_content$error)){
+    if(cvr_content$error == "NOT_FOUND"){
+      stop(paste0(search, " Not Found"))
+    } else if(cvr_content$error == "INVALID_VAT"){
+      stop(cvr_content$message)
+    } else {
+      stop("Your IP has most likely been blocked.")
+    }
+  }
 
-  return(list(cvr_url, cvr_response, cvr_content))
+  return(cvr_content)
 }
 
 
